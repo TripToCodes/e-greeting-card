da1d95985fec3757667095b1fdc622374379a635
TripToCodes <iam.somj@gmail.com>
2025-02-19 09:29:17 -0800

    Merge from feature branch

--- app/card/[id]/page.tsx
+++ app/card/[id]/page.tsx
@@ -1,15 +1,10 @@
-import CardFront from "@/components/card-front";
-import CardInside from "@/components/card-inside";
+import Card from "@/components/card";
+import Tiptap from "@/components/tiptap";
 export default function CardDetails() {
   return (
     <div>
-      <div className="flex justify-between p-10 items-center">
-        <div className="flex flex-row">
-          <CardFront category="Birthday" name="Happy Birthday" />
-          <CardInside />
-        </div>
-        <button>Check Out</button>
-      </div>
+      <Card category="Birthday" name="Happy Birthday" />
+      <Tiptap content="Your content here" preview={true} />
     </div>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import CardFront from "@/components/card-front";
+import Card from "@/components/card";
 export default function Home() {
   return (
-    <div className="flex justify-center items-center">
-      <div className="bg-white shadow-md rounded-lg p-4 text-center">
-        <CardFront category="Birthday" name="Happy Birthday" />
-      </div>
+    <div>
+      <Card category="Birthday" name="Happy Birthday" />
     </div>
   );
 }
