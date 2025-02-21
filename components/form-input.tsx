2ac63e6a31e5dfb2ff1b2c7add27e6b7dbd4d12b
TripToCodes <iam.somj@gmail.com>
2025-02-20 16:26:59 -0800

    Create form components for Login

---
+++ components/form-input.tsx
@@ -0,0 +1,40 @@
+interface FormInputProps {
+  type: string;
+  placeholder: string;
+  required: boolean;
+  errors: string[];
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name: string;
+}
+
+export default function FormInput({
+  type,
+  placeholder,
+  required,
+  errors,
+  name,
+  value,
+  onChange,
+}: FormInputProps) {
+  return (
+    <div className="flex flex-col gap-2">
+      <input
+        name={name}
+        className="bg-transparent rounded-md w-full h-10 focus:outline-none ring-1 focus:ring-2 ring-neutral-200 focus:ring-red-600 border-none placeholder:text-neutral-400"
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        required={required}
+      />
+      {errors.map((error, index) => {
+        return (
+          <span key={index} className="text-red-500 font-medium">
+            {error}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
